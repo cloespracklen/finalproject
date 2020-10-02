2362a7037d5e78c76dfb51a8823443f9d1a8c253
cloespracklen <cloe.spracklen@centre.edu>
2020-10-02 11:59:17 -0400

    finishing this up need to fix hover buttons

--- finalproject.js
+++ finalproject.js
@@ -2,7 +2,7 @@
 var drawMap= function(mapData,target,pathGen,projection, border, field)
 {
     var color1= d3.scaleQuantize()
-    .range(["rgb(204,230,255)","rgb(102,181,255)","rgb(0,92,179)", "rgb(0,40,77)"])
+    .range(["rgb(255,255,255)","rgb(230,247,255)","rgb(179,231,255)", "rgb(128,215,255)", "rgb(77,198,255)","rgb(26,182,255)","rgb(0,157,230)", "rgb(0,122,179)", "rgb(0,87,128)","rgb(0,52,77)", "rgb(0,0,0)"])
     .domain([.0402,.4443]);
     
       var color2= d3.scaleQuantize()
@@ -30,20 +30,22 @@ var drawMap= function(mapData,target,pathGen,projection, border, field)
     
     }
           )
-          
+         .style("stroke-width", 3) 
     .style("stroke", function(d)
           {
        if(d.properties.data)
         {
+            
              var value= d.properties.data[border];
            return color2(value); 
+            
         }
         else
         {
             return "#ccc";
         }
     })
- .on("mouseenter" ,function(mapData, target, pathGen, projection, border, field)
+ .on("mouseenter" ,function(mapData)
       {
         
       var xPos = d3.event.pageX;
@@ -54,8 +56,10 @@ var drawMap= function(mapData,target,pathGen,projection, border, field)
         .style("top",yPos+"px")
         .style("left",xPos+"px")
         
+        d3.select("#geoData")
+            .text(mapData.properties.data[border]);
         d3.select("#mapData")
-        .text(mapData.properties.data["incidence_rates"]);
+        .text(mapData.properties.data[field]);
         
       })//tool tip off
     .on("mouseleave",function()
@@ -74,7 +78,7 @@ var makeTranslateString=function(x,y)
 
 var initGraph= function(stateData, mapData)
 {
-    var screen = {width:1000,height:800}
+    var screen = {width:1000,height:550}
     
     var margins =
         {left:30,right:20,top:20,bottom:30}
@@ -100,16 +104,6 @@ graph,target)
         .attr("x",margins.left+(graph.width/2))
         .attr("y",margins.top)
 }
-  
- 
- 
- 
- 
- 
- 
- 
- 
- 
  
  // joining the data from json and csv DO NOT put anything below this line unless in sucessFCN // 
  
@@ -158,6 +152,8 @@ var pathGen= d3.geoPath()
         drawMap(mapData, target, pathGen, projection, "nohealthcare_rates", "incidence_rates");
     
     drawMap(mapData, target, pathGen, projection, "nohealthcare_rates", "mortality_rates")
+    
+    drawMapbutton(mapData,target,pathGen,projection)
 }
 
 
@@ -165,29 +161,30 @@ var pathGen= d3.geoPath()
 
 
 //--------buttons----------
-		var drawMapbutton = function (mapData,target,pathGen,projection, border, field)
+		var drawMapbutton = function (mapData,target,pathGen,projection)
 		{
 			d3.select("#banner1")
 			.on("click", function()
 			   {
                 console.log("hello")
-				
+				d3.selectAll("path")
 				.remove()
 			drawMap(mapData, target, pathGen, projection, "nohealthcare_rates", "incidence_rates");
 				
 			})
-        }
+        
 		{
 			d3.select("#banner2")
 			.on("click", function()
 			   {
                 console.log("hi")
+                d3.selectAll("path")
 				.remove()
                 
 			drawMap(mapData, target, pathGen, projection, "nohealthcare_rates", "mortality_rates")
 				
 			})
-        }
+        }}
 				
 		
 
@@ -196,6 +193,8 @@ var successFCN = function(states)
 {
     console.log("states", states);
     initGraph(states[0], states[1]);
+   
+    
     
 }
 var failureFCN= function(error)
